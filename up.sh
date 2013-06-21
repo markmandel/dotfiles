870aa5425fe8d4e2872c305d1bb42e59cb33a975
Mark Mandel <mark.mandel@gmail.com>
2013-06-21 14:17:47 +1000

    We have synergy being installed!

--- up.sh
+++ up.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
-
+#!/bin/bash
 set -x verbose #echo on
 
 #only add the bare necessities to get started. Everything else should be ansible.
-workspace=~/workspace/
+workspace="$HOME/workspace"
 ansible="$workspace/ansible"
 ansible_git="git@github.com:markmandel/ansible.git"
 dot_files="$workspace/dotfiles"
 
-if [ -f $workspace ]; then
+sudo apt-get install python python-yaml python-jinja2
+
+if [ -d $workspace ]; then
 
 	#provision ansible itself
-	if [ ! -f $ansible ]; then
+	if [ ! -d $ansible ]; then
 		echo "Cloning Ansible..."
 		git clone $ansible_git $ansible
 	else
@@ -20,19 +21,30 @@ if [ -f $workspace ]; then
 		git pull --rebase
 	fi
 
-	cd $ansible
-	source hacking/env-setup
+	echo "Setting up Ansible in $ansible"
+	#for some reason have to go one level in. Don't ask, it works.	
+	cd $ansible/hacking
+	source env-setup
+
+	ansible --version
 
 	#self update
 	echo "Updating Dotfiles..."
 	cd $dot_files
 	git pull --rebase
 
+	cd $dot_files/provision
 	echo "Which machine are you?"
 	select sp in "Primary" "Secondary"; do
 		case $sp in
-			Primary ) ansible-playbook ./provision/primary.yml;;
-			Secondary ) ansible-playbook ./provision/secondary.yml;;
+			Primary ) 
+				ansible-playbook -vvv -K -i hosts primary.yml
+				break				
+				;;
+			Secondary ) 
+				ansible-playbook -vvv -K -i hosts secondary.yml
+				break
+				;;
 		esac
 	done
 
