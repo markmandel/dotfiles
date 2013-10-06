e514ca6744f404e85792517671e68e69edcc2175
Mark Mandel <mark.mandel@gmail.com>
2013-10-06 18:37:47 +1100

    Goto window for xmonad.

--- provision/roles/xmonad/files/xmonad.hs
+++ provision/roles/xmonad/files/xmonad.hs
@@ -1,10 +1,13 @@
 import XMonad
 import XMonad.Config.Gnome
 import qualified Data.Map as M
+import XMonad.Actions.WindowBringer
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 	[  ((modm, xK_p), spawn "dmenu_run") 
-	, ((modm .|. shiftMask, xK_p), spawn "p=`find -type d | dmenu -l 20` && nautilus $p") ]
+	, ((modm .|. shiftMask, xK_p), spawn "p=`find -type d | dmenu -l 20` && nautilus $p")
+	, ((modm .|. shiftMask, xK_g     ), gotoMenu)
+	, ((modm .|. shiftMask, xK_b     ), bringMenu) ]
 
 main = xmonad gnomeConfig {
 	modMask = mod4Mask,
