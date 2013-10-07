fbc14dc93aeb0afd68605b768829bc6fda6fbc0b
Mark Mandel <mark.mandel@gmail.com>
2013-10-07 14:19:21 +1100

    More tweaking of xmonad.

--- provision/roles/xmonad/files/xmonad.hs
+++ provision/roles/xmonad/files/xmonad.hs
@@ -4,10 +4,10 @@ import qualified Data.Map as M
 import XMonad.Actions.WindowBringer
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-	[  ((modm, xK_p), spawn "dmenu_run") 
-	, ((modm .|. shiftMask, xK_p), spawn "p=`find -type d | dmenu -l 20` && nautilus $p")
-	, ((modm .|. shiftMask, xK_g     ), gotoMenu)
-	, ((modm .|. shiftMask, xK_b     ), bringMenu) ]
+	[  ((modm, xK_p), spawn "dmenu_run -l 20 -fn ubuntu-mono-10") 
+	, ((modm .|. shiftMask, xK_p), spawn "p=`find -type d | dmenu -fn ubuntu-mono-10 -l 20` && nautilus $p")
+	, ((modm .|. shiftMask, xK_g     ), gotoMenuArgs ["-fn", "ubuntu-mono-10", "-l", "20"])
+	, ((modm .|. shiftMask, xK_b     ), bringMenuArgs ["-fn", "ubuntu-mono-10", "-l", "20"]) ]
 
 main = xmonad gnomeConfig {
 	modMask = mod4Mask,
