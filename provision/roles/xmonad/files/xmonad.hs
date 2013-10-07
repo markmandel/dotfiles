50a1a8c6252c05fcf631519f202cabf5c279d3e5
Mark Mandel <mark.mandel@gmail.com>
2013-10-08 10:56:34 +1100

    More xmonad updates.

--- provision/roles/xmonad/files/xmonad.hs
+++ provision/roles/xmonad/files/xmonad.hs
@@ -1,7 +1,9 @@
 import XMonad
 import XMonad.Config.Gnome
 import qualified Data.Map as M
+import XMonad.Hooks.ManageHelpers
 import XMonad.Actions.WindowBringer
+
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 	[  ((modm, xK_p), spawn "dmenu_run -l 20 -fn ubuntu-mono-10") 
@@ -9,7 +11,13 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 	, ((modm .|. shiftMask, xK_g     ), gotoMenuArgs ["-fn", "ubuntu-mono-10", "-l", "20"])
 	, ((modm .|. shiftMask, xK_b     ), bringMenuArgs ["-fn", "ubuntu-mono-10", "-l", "20"]) ]
 
+myManageHook = 
+	[
+		className =? "Guake" --> doFloat
+	]
+
 main = xmonad gnomeConfig {
 	modMask = mod4Mask,
-	keys = myKeys <+> keys gnomeConfig
+	keys = myKeys <+> keys gnomeConfig,
+	manageHook = manageHook gnomeConfig <+> composeAll myManageHook
 }
