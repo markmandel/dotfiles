53cf58a16fb7c5fff2d2121b072606e1a3966e18
Mark Mandel <mark.mandel@gmail.com>
2013-10-10 14:57:12 +1100

    Switch over to EZConfig and allow the use of the number pad to switch windows.

--- provision/roles/xmonad/files/xmonad.hs
+++ provision/roles/xmonad/files/xmonad.hs
@@ -1,26 +1,60 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Actions.WindowBringer
+import XMonad.Actions.GridSelect
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
+import XMonad.Layout.NoBorders
+import XMonad.Util.EZConfig
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-	[  ((modm, xK_p), spawn "dmenu_run -l 20 -fn ubuntu-mono-10") 
-	, ((modm .|. shiftMask, xK_p), spawn "p=`find -type d | dmenu -fn ubuntu-mono-10 -l 20` && nautilus $p")
-	, ((modm .|. shiftMask, xK_g     ), gotoMenuArgs ["-fn", "ubuntu-mono-10", "-l", "20"])
-	, ((modm .|. shiftMask, xK_b     ), bringMenuArgs ["-fn", "ubuntu-mono-10", "-l", "20"]) ]
+myWorkspaces = ["1","2","3","4","5","6","7","8","9","0"]
+
+myModKey = mod4Mask
+
+-- Non-numeric num pad keys, sorted by number 
+numPadKeys = [ xK_KP_End,  xK_KP_Down,  xK_KP_Page_Down -- 1, 2, 3
+             , xK_KP_Left, xK_KP_Begin, xK_KP_Right     -- 4, 5, 6
+             , xK_KP_Home, xK_KP_Up,    xK_KP_Page_Up   -- 7, 8, 9
+             , xK_KP_Insert] -- 0
+
+-- general keys
+myKeys =[  ((myModKey, xK_p), spawn "dmenu_run -l 20 -fn ubuntu-mono-10") 
+	, ((myModKey .|. shiftMask, xK_p), spawn "p=`find -type d | dmenu -fn ubuntu-mono-10 -l 20` && nautilus $p")
+	, ((myModKey .|. shiftMask, xK_g     ), gotoMenuArgs ["-fn", "ubuntu-mono-10", "-l", "20"])
+	, ((myModKey, xK_g), goToSelected defaultGSConfig)
+	, ((myModKey .|. shiftMask, xK_b     ), bringMenuArgs ["-fn", "ubuntu-mono-10", "-l", "20"]) ]
+	++
+	-- numberpad
+	[((m .|. myModKey, k), windows $ f i)
+		| (i, k) <- zip myWorkspaces numPadKeys
+		, (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+	]
 
 myManageHook = 
 	[
 		className =? "Guake" --> doFloat
+		,isFullscreen --> doFullFloat
 	]
 
-main = xmonad gnomeConfig {
-	modMask = mod4Mask,
-	keys = myKeys <+> keys gnomeConfig,
-	manageHook = manageHook gnomeConfig <+> composeAll myManageHook
+main = xmonad $ gnomeConfig {
+	modMask = myModKey
+	,workspaces = myWorkspaces
+	,layoutHook = smartBorders (layoutHook gnomeConfig)
+	,manageHook = manageHook gnomeConfig <+> composeAll myManageHook
 	,startupHook = do
            startupHook gnomeConfig
            setWMName "LG3D"
-}
+} `additionalKeys` myKeys
+
+{-|
+	## TODO list ##
+	- make the 0 workspace work
+	- work out what is up with Keypass
+	- scratchpad to replace guake
+	- scratchpad for Keepass
+	- dmenu open-terminal?
+-}
+
+
