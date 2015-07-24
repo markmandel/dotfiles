bbbf81c0a23317bf88f484707ffd4f3b7288a2e7
Mark Mandel <mark.mandel@gmail.com>
2015-07-23 22:23:27 -0700

    Better functions for zsh.

--- sandboxes/gcloudshell/include.zsh
+++ sandboxes/gcloudshell/include.zsh
@@ -2,13 +2,5 @@
 
 #includes for go zsh config
 function gcloudshell() {
-
-    docker run --rm \
-	    -v ~/.oh-my-zsh:/root/.oh-my-zsh \
-	    -v ~/.dircolors:/root/.dircolors \
-	    -v ~/.zsh_history:/root/.zsh_history \
-	    -v $SANDBOXES/gcloudshell/zshrc:/root/.zshrc \
-	    -v ~/.config:/root/.config \
-	    -v `pwd`:/go \
-	    -it markmandel/gcloudshell zsh
+	_run_docker_zsh 'gcloudshell'
 }
--- sandboxes/include.zsh
+++ sandboxes/include.zsh
@@ -4,10 +4,28 @@
 source $SANDBOXES/goshell/include.zsh
 source $SANDBOXES/gcloudshell/include.zsh
 
+function _run_docker_zsh() {
+    local shell=$1
+    local dir=$shell
+
+    #if here is a second value, use that as the dir
+    if [ -n "$2" ]; then dir=$2; fi
+
+    docker run --rm \
+	    -v ~/.oh-my-zsh:/root/.oh-my-zsh \
+	    -v ~/.dircolors:/root/.dircolors \
+	    -v ~/.zsh_history:/root/.zsh_history \
+	    -e TERM=$TERM \
+	    -v $SANDBOXES/goshell/zshrc:/root/.zshrc \
+        #TODO: pass in /where to mount as a parameter
+	    -v `pwd`:/go \
+	    -it markmandel/goshell zsh
+}
+
 #Credit: https://github.com/jbbarth/dotfiles/blob/master/.zsh/docker.zsh
 function docker-cleanup() {
-  echo "* Removing old containers"
-  docker ps -qa | xargs --no-run-if-empty -n 1 docker rm
-  echo "* Removing <none> images"
-  docker images --filter dangling=true -q | xargs --no-run-if-empty -n 1 docker rmi
+    echo "* Removing old containers"
+    docker ps -qa | xargs --no-run-if-empty -n 1 docker rm
+    echo "* Removing <none> images"
+    docker images --filter dangling=true -q | xargs --no-run-if-empty -n 1 docker rmi
 }
