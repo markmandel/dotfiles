51afc34625616048ce09ab88328700aa899676d5
Mark Mandel <markmandel@google.com>
2015-07-30 21:29:26 -0700

    Not really working :/

--- sandboxes/scudcloud/include.zsh
+++ sandboxes/scudcloud/include.zsh
@@ -1,12 +1,20 @@
 #!/usr/bin/env zsh
 
 #includes for go zsh config
-function scudcloud() {
+function slack() {
     mkdir -p ~/.config/scudcloud
-    docker run \
+    docker run --rm \
+            --name scudcloud \
             -e HOST_GID=`id -g` \
             -e HOST_UID=`id -u` \
             -e HOST_USER=$USER \
+            -e DISPLAY \
+            -e DBUS_SESSION_BUS_ADDRESS \
+            -e XAUTHORITY \
+            -v /tmp/.X11-unix:/tmp/.X11-unix \
             -v ~/.config/scudcloud:/home/$USER/.config/scudcloud \
+            -v ~/.Xauthority:/home/$USER/.Xauthority \
+            -v /var/lib/dbus/machine-id:/var/lib/dbus/machine-id:ro \
+            -v /var/run/dbus:/var/run/dbus \
             markmandel/scudcloud
 }
