1bb7742618ac1f2778070772d2eb0a5711e7372e
Mark Mandel <mark.mandel@gmail.com>
2015-08-15 14:11:49 -0700

    Fixup for ghc env. Tricky one.

--- sandboxes/core.zsh
+++ sandboxes/core.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-coreplugins=(git git-extras colored-man autojump zsh_reload dirhistory zsh-syntax-highlighting)
+coreplugins=(theme git git-extras colored-man autojump zsh_reload dirhistory zsh-syntax-highlighting)
 
--- sandboxes/ghcshell/include.zsh
+++ sandboxes/ghcshell/include.zsh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
 
 function ghcshell() {
-    mkdir -p ~/.ghcshell/cabal
-    mkdir -p ~/.ghcshell/ghc
+    mkdir -p ~/.cabal
+    mkdir -p ~/.ghc
     _docker_zsh "ghcshell" "/project" \
-                "-v ~/.ghcshell/cabal:/home/$USER/.cabal" \
-                "-v ~/.ghcshell/ghc:/home/$USER/.ghc"
+                "-v ~/.cabal:/home/$USER/.cabal" \
+                "-v ~/.ghc:/home/$USER/.ghc"
+}
+
+function clean-ghcshell {
+    rm -r ~/.cabal
+    rm -r ~/.ghc
 }
