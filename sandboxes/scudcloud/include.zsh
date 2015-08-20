f1a55f13e0d4a2c8281a0802cf0c27079e5c1784
Mark Mandel <markmandel@google.com>
2015-08-20 12:13:13 -0700

    IT ALIVE! Scudcloud works!

--- sandboxes/scudcloud/include.zsh
+++ sandboxes/scudcloud/include.zsh
@@ -1,45 +1,24 @@
 #!/usr/bin/env zsh
 
-#docker run -d \
-#		-v /tmp/.X11-unix:/tmp/.X11-unix \
-#		-e DISPLAY=unix$DISPLAY \
-#		-v /etc/machine-id:/etc/machine-id:ro \
-#		-v /var/run/dbus:/var/run/dbus \
-#		-v /var/run/user/$(id -u):/var/run/user/$(id -u) \
-#		-e TERM \
-#		-e XAUTHORITY \
-#		-e DBUS_SESSION_BUS_ADDRESS \
-#		-e HOME \
-#		-e QT_DEVICE_PIXEL_RATIO \
-#		-v /etc/passwd:/etc/passwd:ro \
-#		-v /etc/group:/etc/group:ro \
-#		-u $(whoami) -w "$HOME" \
-#		-v $HOME/.Xauthority:$HOME/.Xauthority \
-#		-v /etc/machine-id:/etc/machine-id:ro \
-#		-v $HOME/.scudcloud:/home/jessie/.config/scudcloud \
-#		--device /dev/snd \
-#		--name slack \
-#		jess/slack
-
 #includes for go zsh config
 function slack() {
     mkdir -p ~/.config/scudcloud
     docker run --rm -it \
-            --privileged \
-            --name scudcloud \
-            --device /dev/snd \
-            --privileged \
-            -e DISPLAY \
-            -e XAUTHORITY \
-            -e DBUS_SESSION_BUS_ADDRESS \
-            -u $USER \
-            -v /etc/localtime:/etc/localtime:ro \
-            -v /tmp/.X11-unix:/tmp/.X11-unix \
-            -v /etc/passwd:/etc/passwd:ro \
-            -v /etc/group:/etc/group:ro \
-            -v $HOME/.Xauthority:$HOME/.Xauthority \
-            -v $HOME/.config/scudcloud:/home/$USER/.config/scudcloud \
-            -v /var/run/dbus:/var/run/dbus \
-            -v /var/run/user/$(id -u):/var/run/user/$(id -u) \
-            markmandel/scudcloud
+	    --privileged \
+	    --net=host \
+	    --device=/dev/snd \
+		-e DISPLAY \
+		-e QT_GRAPHICSSYSTEM=native \
+		-e XAUTHORITY \
+		-e DBUS_SESSION_BUS_ADDRESS \
+		-u $USER \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/group:/etc/group:ro \
+        -v $HOME/.Xauthority:$HOME/.Xauthority \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		-v /var/run/dbus:/var/run/dbus \
+        -v /var/lib/dbus/machine-id:/var/lib/dbus/machine-id \
+		-v $HOME/.config/scudcloud:/home/$USER/.config/scudcloud \
+		-v /usr/share/themes:/usr/share/themes \
+		markmandel/scudcloud
 }
