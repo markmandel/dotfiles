692e9c8ee12d31b44168ccb3dc2d0a0cfd4ea856
Mark Mandel <markmandel@google.com>
2015-08-27 19:20:58 -0700

    Some nicer handling of how slack GUI starts up.

--- sandboxes/scudcloud/include.zsh
+++ sandboxes/scudcloud/include.zsh
@@ -2,26 +2,27 @@
 
 #includes for go zsh config
 function slack() {
-	mkdir -p /tmp/scudhome/.config/scudcloud
+    mkdir -p /tmp/scudhome/.config/scudcloud
     mkdir -p ~/.config/scudcloud
     docker run -d \
-	    --privileged \
-	    --net=host \
-	    --device=/dev/snd \
-		-e DISPLAY \
-		-e QT_GRAPHICSSYSTEM=native \
-		-e XAUTHORITY \
-		-e DBUS_SESSION_BUS_ADDRESS \
-		-u $USER \
+        --name=scudcloud \
+        --privileged \
+        --net=host \
+        --device=/dev/snd \
+        -e DISPLAY \
+        -e QT_GRAPHICSSYSTEM=native \
+        -e XAUTHORITY \
+        -e DBUS_SESSION_BUS_ADDRESS \
+        -u $USER \
         -v /etc/passwd:/etc/passwd:ro \
         -v /etc/group:/etc/group:ro \
         -v /etc/localtime:/etc/localtime \
         -v $HOME/.Xauthority:$HOME/.Xauthority \
-		-v /tmp/.X11-unix:/tmp/.X11-unix \
-		-v /var/run/dbus:/var/run/dbus \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v /var/run/dbus:/var/run/dbus \
         -v /var/lib/dbus/machine-id:/var/lib/dbus/machine-id \
         -v /tmp/scudhome:$HOME \
-		-v $HOME/.config/scudcloud:/home/$USER/.config/scudcloud \
-		-v /usr/share/themes:/usr/share/themes \
-		markmandel/scudcloud
+        -v $HOME/.config/scudcloud:/home/$USER/.config/scudcloud \
+        -v /usr/share/themes:/usr/share/themes \
+        markmandel/scudcloud
 }
