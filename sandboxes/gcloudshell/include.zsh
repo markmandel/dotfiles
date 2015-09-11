c62337b1e8704a5108860f2f3aa21a95bdf97f20
Mark Mandel <markmandel@google.com>
2015-09-11 14:51:47 -0700

    Improvements on the gcloud shell.

--- sandboxes/gcloudshell/include.zsh
+++ sandboxes/gcloudshell/include.zsh
@@ -3,12 +3,14 @@
 #includes for go zsh config
 function gcloudshell() {
     mkdir -p ~/.config/gcloud
+    touch ~/.appcfg_oauth2_tokens
     typeset -a dockerGroup
     dockerGroup=(${(@s/:/)$(getent group docker)})
 
-	_docker_zsh 'gcloudshell' '/go' \
-	            "-e DOCKER_GID=$dockerGroup[3]" \
-				"-v ~/.config/gcloud:/home/$USER/.config/gcloud" \
-				"-v /usr/bin/docker:/usr/bin/docker" \
-				"-v /var/run/docker.sock:/var/run/docker.sock"
+    _docker_zsh 'gcloudshell' '/go' \
+                "-e DOCKER_GID=$dockerGroup[3]" \
+                "-v ~/.appcfg_oauth2_tokens:/home/$USER/.appcfg_oauth2_tokens" \
+                "-v ~/.config/gcloud:/home/$USER/.config/gcloud" \
+                "-v /usr/bin/docker:/usr/bin/docker" \
+                "-v /var/run/docker.sock:/var/run/docker.sock"
 }
