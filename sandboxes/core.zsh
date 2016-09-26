c67289a78f649cef447d6f0e6b5bbfd882336695
Mark Mandel <markmandel@google.com>
2016-09-26 11:21:03 -0700

    Add zsh-autosuggestions as a plugin.

--- sandboxes/core.zsh
+++ sandboxes/core.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-coreplugins=(themes git git-extras colored-man autojump zsh_reload dirhistory zsh-syntax-highlighting)
+coreplugins=(themes git git-extras colored-man autojump zsh_reload dirhistory zsh-syntax-highlighting zsh-autosuggestions)
 
