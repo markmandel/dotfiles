559c153ecfaabcc20f791a7fb7d849a251823336
Mark Mandel <mark@compoundtheory.com>
2025-04-16 09:28:53 -0700

    Add ssh-agent grab, since we have a private github repo

--- up.sh
+++ up.sh
@@ -14,22 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#set -x verbose #echo on
+set -euo pipefail
 
 #only add the bare necessities to get started. Everything else should be ansible.
 workspace="$HOME/workspace"
 dot_files="$workspace/dotfiles"
 
-## Grab the Debian version information as env vars
-#set -o allexport
-#source /etc/os-release
-#set +o allexport
-#
-#echo "Debian Version: ${VERSION_CODENAME}"
+# Check if ssh-add has been run and run it if needed
+if ! ssh-add -l &>/dev/null; then
+  echo "Running ssh-add to add SSH keys to the agent..."
+  ssh-add
+fi
 
 playbook="${1:-ui}"
 
-cd $dot_files/provision
+cd "$dot_files/provision"
 echo "Playbook: $playbook.yaml"
 
 ansible-playbook -vvv -K -i hosts "$playbook.yaml" -e ansible_python_interpreter=/usr/bin/python3
