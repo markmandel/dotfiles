5383c637ca0c21d5f96e478a80ca81eadc8e6c6e
Mark Mandel <mark@compoundtheory.com>
2026-05-25 18:40:50 -0700

    Starting to migrate some of the lua

---
+++ provision/ui/files/config/hypr/includes/rose-pine.lua
@@ -0,0 +1,40 @@
+--[[
+Copyright 2025 Mark Mandel All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]
+
+-- Original: https://github.com/jishnurajendran/hyprland-rosepine/blob/main/rose-pine.conf
+-- name: Rosé Pine
+-- author: jishnurajendran
+-- All natural pine, faux fur and a bit of soho vibes for the classy minimalist
+
+local M = {}
+
+M.base          = "191724"
+M.surface       = "1f1d2e"
+M.overlay       = "26233a"
+M.muted         = "6e6a86"
+M.subtle        = "908caa"
+M.text          = "e0def4"
+M.love          = "eb6f92"
+M.gold          = "f6c177"
+M.rose          = "ebbcba"
+M.pine          = "31748f"
+M.foam          = "9ccfd8"
+M.iris          = "c4a7e7"
+M.highlightLow  = "21202e"
+M.highlightMed  = "403d52"
+M.highlightHigh = "524f67"
+
+return M
---
+++ provision/ui/files/config/hypr/includes/theme.lua
@@ -0,0 +1,106 @@
+--[[
+Copyright 2025 Mark Mandel All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]
+
+---------------------
+--- LOOK AND FEEL ---
+---------------------
+
+local colours = require("rose-pine")
+
+-- Refer to https://wiki.hypr.land/Configuring/Variables/
+
+-- https://wiki.hypr.land/Configuring/Variables/#general
+hl.config({
+    general = {
+        gaps_in     = 2,
+        gaps_out    = 2,
+        border_size = 2,
+
+        -- https://wiki.hypr.land/Configuring/Variables/#variable-types for info about colors
+        col = {
+            active_border   = "rgb(" .. colours.love .. ")",
+            inactive_border = "rgb(" .. colours.muted .. ")",
+        },
+
+        -- Set to true enable resizing windows by clicking and dragging on borders and gaps
+        resize_on_border = false,
+
+        -- Please see https://wiki.hypr.land/Configuring/Tearing/ before you turn this on
+        allow_tearing = false,
+    },
+
+    -- https://wiki.hypr.land/Configuring/Variables/#decoration
+    decoration = {
+        rounding = 5,
+
+        -- Change transparency of focused and unfocused windows
+        active_opacity   = 1.0,
+        inactive_opacity = 1.0,
+
+        shadow = {
+            enabled      = true,
+            range        = 4,
+            render_power = 3,
+            color        = "rgb(" .. colours.surface .. ")",
+        },
+
+        -- https://wiki.hypr.land/Configuring/Variables/#blur
+        blur = {
+            enabled = false,
+        },
+    },
+})
+
+-- https://wiki.hypr.land/Configuring/Variables/#animations
+hl.config({
+    animations = {
+        enabled = true,
+    },
+})
+
+-- Default animations, see https://wiki.hypr.land/Configuring/Animations/ for more
+
+hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}  } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}  } })
+hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}     } })
+hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}  } })
+hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}   } })
+
+hl.animation({ leaf = "global",        enabled = true, speed = 10,   bezier = "default"       })
+hl.animation({ leaf = "border",        enabled = true, speed = 5.39, bezier = "easeOutQuint"  })
+hl.animation({ leaf = "windows",       enabled = true, speed = 4.79, bezier = "easeOutQuint"  })
+hl.animation({ leaf = "windowsIn",     enabled = true, speed = 4.1,  bezier = "easeOutQuint",  style = "popin 87%" })
+hl.animation({ leaf = "windowsOut",    enabled = true, speed = 1.49, bezier = "linear",        style = "popin 87%" })
+hl.animation({ leaf = "fadeIn",        enabled = true, speed = 1.73, bezier = "almostLinear"  })
+hl.animation({ leaf = "fadeOut",       enabled = true, speed = 1.46, bezier = "almostLinear"  })
+hl.animation({ leaf = "fade",          enabled = true, speed = 3.03, bezier = "quick"         })
+hl.animation({ leaf = "layers",        enabled = true, speed = 1.5,  bezier = "easeOutQuint"  })
+hl.animation({ leaf = "layersIn",      enabled = true, speed = 1,    bezier = "linear",        style = "fade" })
+hl.animation({ leaf = "layersOut",     enabled = true, speed = 1,    bezier = "linear",        style = "fade" })
+hl.animation({ leaf = "fadeLayersIn",  enabled = true, speed = 1.79, bezier = "almostLinear"  })
+hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, bezier = "almostLinear"  })
+hl.animation({ leaf = "workspaces",    enabled = true, speed = 1.94, bezier = "almostLinear",  style = "fade" })
+hl.animation({ leaf = "workspacesIn",  enabled = true, speed = 1.21, bezier = "almostLinear",  style = "fade" })
+hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear",  style = "fade" })
+
+-- https://wiki.hypr.land/Configuring/Variables/#misc
+hl.config({
+    misc = {
+        force_default_wallpaper  = 0,    -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo    = true, -- If true disables the random hyprland logo / anime girl background. :(
+        disable_splash_rendering = true,
+    },
+})
